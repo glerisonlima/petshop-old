f786c8fcb484cbc1970a189a12a0f8620e280bff
glerison <glerisonlima@gmail.com>
2018-03-16 00:26:49 -0300

    criando busca de categoria por id

--- src/main/java/br/com/petshop/resources/CategoriaResource.java
+++ src/main/java/br/com/petshop/resources/CategoriaResource.java
@@ -3,25 +3,28 @@ package br.com.petshop.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petshop.domain.Categoria;
+import br.com.petshop.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1,"Escritório");
-		Categoria cat2 = new Categoria(2,"Informática");
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 		
-		return lista;
 	}
 }
---
+++ src/main/java/br/com/petshop/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package br.com.petshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.petshop.domain.Categoria;
+import br.com.petshop.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
