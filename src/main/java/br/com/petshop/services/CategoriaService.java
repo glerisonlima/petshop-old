f136e59f947bbec339a8ec8eff28b3135008f0ef
glerison <glerisonlima@gmail.com>
2018-03-17 00:17:43 -0300

    ajuste no end point /categoria/id

--- src/main/java/br/com/petshop/services/CategoriaService.java
+++ src/main/java/br/com/petshop/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.petshop.domain.Categoria;
 import br.com.petshop.repositories.CategoriaRepository;
+import br.com.petshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
